7006db9075a217bcf41745f4ac0c9b50f11f971b
SwthSawTheme <sawtheme1@gmail.com>
2024-06-15 13:28:05 -0300

    Adicionado medidor de tempo

--- src/doc.c
+++ src/doc.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void){
 
-    int num = 3;
+    clock_t start, end;
+    double cpu_time_used;
+
+    int num = 0;
+
+    printf("Digite um valor para saber se é Par ou Impar\n");
+    scanf("%d",&num);
+
+    start = clock();
 
     if (num % 2 == 0){
         printf("Par");
     } else{
         printf("Impar");
     }
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("\nTempo de execução: %f segundos\n", cpu_time_used);
+
     return 0;
 }
